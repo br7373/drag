6eff8fab717a99c0c0ad98fbbc641ec423f2d7f9
br7373 <46458606+br7373@users.noreply.github.com>
2019-01-29 13:08:31 -0600

    Add files via upload

---
+++ Drag and Drop 2/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lbl2x1 = New System.Windows.Forms.Label()
+        Me.lbl2x2 = New System.Windows.Forms.Label()
+        Me.lbl2x3 = New System.Windows.Forms.Label()
+        Me.lbl2x4 = New System.Windows.Forms.Label()
+        Me.lbl2x5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl8 = New System.Windows.Forms.Label()
+        Me.lbl10 = New System.Windows.Forms.Label()
+        Me.lbl6 = New System.Windows.Forms.Label()
+        Me.lblCounter = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbl2x1
+        '
+        Me.lbl2x1.AutoSize = True
+        Me.lbl2x1.Location = New System.Drawing.Point(88, 41)
+        Me.lbl2x1.Name = "lbl2x1"
+        Me.lbl2x1.Size = New System.Drawing.Size(23, 13)
+        Me.lbl2x1.TabIndex = 0
+        Me.lbl2x1.Text = "2*1"
+        '
+        'lbl2x2
+        '
+        Me.lbl2x2.AutoSize = True
+        Me.lbl2x2.Location = New System.Drawing.Point(88, 69)
+        Me.lbl2x2.Name = "lbl2x2"
+        Me.lbl2x2.Size = New System.Drawing.Size(23, 13)
+        Me.lbl2x2.TabIndex = 1
+        Me.lbl2x2.Text = "2*2"
+        '
+        'lbl2x3
+        '
+        Me.lbl2x3.AutoSize = True
+        Me.lbl2x3.Location = New System.Drawing.Point(88, 97)
+        Me.lbl2x3.Name = "lbl2x3"
+        Me.lbl2x3.Size = New System.Drawing.Size(23, 13)
+        Me.lbl2x3.TabIndex = 2
+        Me.lbl2x3.Text = "2*3"
+        '
+        'lbl2x4
+        '
+        Me.lbl2x4.AutoSize = True
+        Me.lbl2x4.Location = New System.Drawing.Point(88, 127)
+        Me.lbl2x4.Name = "lbl2x4"
+        Me.lbl2x4.Size = New System.Drawing.Size(23, 13)
+        Me.lbl2x4.TabIndex = 3
+        Me.lbl2x4.Text = "2*4"
+        '
+        'lbl2x5
+        '
+        Me.lbl2x5.AutoSize = True
+        Me.lbl2x5.Location = New System.Drawing.Point(88, 153)
+        Me.lbl2x5.Name = "lbl2x5"
+        Me.lbl2x5.Size = New System.Drawing.Size(23, 13)
+        Me.lbl2x5.TabIndex = 4
+        Me.lbl2x5.Text = "2*5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(110, 196)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Correct :"
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Location = New System.Drawing.Point(165, 153)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(13, 13)
+        Me.lbl4.TabIndex = 11
+        Me.lbl4.Text = "4"
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(165, 127)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(13, 13)
+        Me.lbl2.TabIndex = 10
+        Me.lbl2.Text = "2"
+        '
+        'lbl8
+        '
+        Me.lbl8.AutoSize = True
+        Me.lbl8.Location = New System.Drawing.Point(165, 97)
+        Me.lbl8.Name = "lbl8"
+        Me.lbl8.Size = New System.Drawing.Size(13, 13)
+        Me.lbl8.TabIndex = 9
+        Me.lbl8.Text = "8"
+        '
+        'lbl10
+        '
+        Me.lbl10.AutoSize = True
+        Me.lbl10.Location = New System.Drawing.Point(165, 69)
+        Me.lbl10.Name = "lbl10"
+        Me.lbl10.Size = New System.Drawing.Size(19, 13)
+        Me.lbl10.TabIndex = 8
+        Me.lbl10.Text = "10"
+        '
+        'lbl6
+        '
+        Me.lbl6.AutoSize = True
+        Me.lbl6.Location = New System.Drawing.Point(165, 41)
+        Me.lbl6.Name = "lbl6"
+        Me.lbl6.Size = New System.Drawing.Size(13, 13)
+        Me.lbl6.TabIndex = 7
+        Me.lbl6.Text = "6"
+        '
+        'lblCounter
+        '
+        Me.lblCounter.AutoSize = True
+        Me.lblCounter.Location = New System.Drawing.Point(71, 248)
+        Me.lblCounter.Name = "lblCounter"
+        Me.lblCounter.Size = New System.Drawing.Size(39, 13)
+        Me.lblCounter.TabIndex = 12
+        Me.lblCounter.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(314, 285)
+        Me.Controls.Add(Me.lblCounter)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl8)
+        Me.Controls.Add(Me.lbl10)
+        Me.Controls.Add(Me.lbl6)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lbl2x5)
+        Me.Controls.Add(Me.lbl2x4)
+        Me.Controls.Add(Me.lbl2x3)
+        Me.Controls.Add(Me.lbl2x2)
+        Me.Controls.Add(Me.lbl2x1)
+        Me.Name = "Form1"
+        Me.Text = "Drag and Drop"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl2x1 As Label
+    Friend WithEvents lbl2x2 As Label
+    Friend WithEvents lbl2x3 As Label
+    Friend WithEvents lbl2x4 As Label
+    Friend WithEvents lbl2x5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lbl4 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl8 As Label
+    Friend WithEvents lbl10 As Label
+    Friend WithEvents lbl6 As Label
+    Friend WithEvents lblCounter As Label
+End Class
---
+++ Drag and Drop 2/Form1.vb
@@ -0,0 +1,211 @@
+﻿Public Class Form1
+    Dim counter As Byte
+    ' Static move As Boolean = True
+    Private Sub lbl6_MouseMove(sender As Object, e As MouseEventArgs) Handles lbl6.MouseMove
+        Static x As Integer
+        Static y As Integer
+        Static move As Boolean = True
+
+        If e.Button = False Then
+            x = e.X
+            y = e.Y
+
+            If lbl6.Bounds.IntersectsWith(lbl2x3.Bounds) Then
+                move = False
+                lbl2x3.ForeColor = Color.Green
+                lbl6.Location = New Point(180, 196)
+                lbl6.ForeColor = Color.Black
+                counter += 1
+                win()
+            End If
+        Else
+            If move = True Then
+                lbl6.Left += (e.X - x)
+                lbl6.Top += (e.Y - y)
+            End If
+        End If
+        If lbl6.Bounds.IntersectsWith(lbl2x1.Bounds) Or lbl6.Bounds.IntersectsWith(lbl2x2.Bounds) Or lbl6.Bounds.IntersectsWith(lbl2x4.Bounds) Or lbl6.Bounds.IntersectsWith(lbl2x5.Bounds) Then
+            lbl6.ForeColor = Color.Red
+            lbl6.Location = New Point(165, 41)
+            '        limit()
+        End If
+        If counter <= 5 Then
+            move = True
+        End If
+        lblCounter.Text = counter
+    End Sub
+
+    Private Sub lbl10_MouseMove(sender As Object, e As MouseEventArgs) Handles lbl10.MouseMove
+        Static x As Integer
+        Static y As Integer
+        Static move As Boolean = True
+
+        If e.Button = False Then
+            x = e.X
+            y = e.Y
+
+            If lbl10.Bounds.IntersectsWith(lbl2x5.Bounds) Then
+                move = False
+                lbl2x5.ForeColor = Color.Green
+                lbl10.Location = New Point(200, 196)
+                lbl10.ForeColor = Color.Black
+                counter += 1
+                '   limit()
+                win()
+            End If
+        Else
+            If move = True Then
+                lbl10.Left += (e.X - x)
+                lbl10.Top += (e.Y - y)
+            End If
+        End If
+        If lbl10.Bounds.IntersectsWith(lbl2x1.Bounds) Or lbl10.Bounds.IntersectsWith(lbl2x2.Bounds) Or lbl10.Bounds.IntersectsWith(lbl2x3.Bounds) Or lbl10.Bounds.IntersectsWith(lbl2x4.Bounds) Then
+            lbl10.ForeColor = Color.Red
+            lbl10.Location = New Point(165, 69)
+            '        limit()
+        End If
+        If counter <= 5 Then
+            move = True
+        End If
+        lblCounter.Text = counter
+    End Sub
+
+    Private Sub lbl8_MouseMove(sender As Object, e As MouseEventArgs) Handles lbl8.MouseMove
+        Static x As Integer
+        Static y As Integer
+        Static move As Boolean = True
+
+        If e.Button = False Then
+            x = e.X
+            y = e.Y
+
+            If lbl8.Bounds.IntersectsWith(lbl2x4.Bounds) Then
+                move = False
+                lbl2x4.ForeColor = Color.Green
+                lbl8.Location = New Point(190, 196)
+                lbl8.ForeColor = Color.Black
+                counter += 1
+                '    limit()
+                win()
+            End If
+        Else
+            If move = True Then
+                lbl8.Left += (e.X - x)
+                lbl8.Top += (e.Y - y)
+            End If
+        End If
+        If lbl8.Bounds.IntersectsWith(lbl2x1.Bounds) Or lbl8.Bounds.IntersectsWith(lbl2x2.Bounds) Or lbl8.Bounds.IntersectsWith(lbl2x3.Bounds) Or lbl8.Bounds.IntersectsWith(lbl2x5.Bounds) Then
+            lbl8.ForeColor = Color.Red
+            lbl8.Location = New Point(165, 97)
+            '        limit()
+        End If
+        If counter <= 5 Then
+            move = True
+        End If
+        lblCounter.Text = counter
+    End Sub
+
+    Private Sub lbl2_MouseMove(sender As Object, e As MouseEventArgs) Handles lbl2.MouseMove
+        Static x As Integer
+        Static y As Integer
+        Static move As Boolean = True
+
+        If e.Button = False Then
+            x = e.X
+            y = e.Y
+
+            If lbl2.Bounds.IntersectsWith(lbl2x1.Bounds) Then
+                move = False
+                lbl2x1.ForeColor = Color.Green
+                lbl2.Location = New Point(160, 196)
+                lbl2.ForeColor = Color.Black
+                counter += 1
+                '      limit()
+                win()
+            End If
+        Else
+            If move = True Then
+                lbl2.Left += (e.X - x)
+                lbl2.Top += (e.Y - y)
+            End If
+        End If
+        If lbl2.Bounds.IntersectsWith(lbl2x2.Bounds) Or lbl2.Bounds.IntersectsWith(lbl2x3.Bounds) Or lbl2.Bounds.IntersectsWith(lbl2x4.Bounds) Or lbl2.Bounds.IntersectsWith(lbl2x5.Bounds) Then
+            lbl2.ForeColor = Color.Red
+            lbl2.Location = New Point(165, 127)
+            '        limit()
+        End If
+        If counter <= 5 Then
+            move = True
+        End If
+        lblCounter.Text = counter
+    End Sub
+
+    Private Sub lbl4_MouseMove(sender As Object, e As MouseEventArgs) Handles lbl4.MouseMove
+        Static x As Integer
+        Static y As Integer
+        Static move As Boolean = True
+
+        If e.Button = False Then
+            x = e.X
+            y = e.Y
+
+            If lbl4.Bounds.IntersectsWith(lbl2x2.Bounds) Then
+                move = False
+                lbl2x2.ForeColor = Color.Green
+                lbl4.Location = New Point(170, 196)
+                lbl4.ForeColor = Color.Black
+                counter += 1
+                '    limit()
+                win()
+            End If
+        Else
+            If move = True Then
+                lbl4.Left += (e.X - x)
+                lbl4.Top += (e.Y - y)
+            End If
+        End If
+        If lbl4.Bounds.IntersectsWith(lbl2x1.Bounds) Or lbl4.Bounds.IntersectsWith(lbl2x3.Bounds) Or lbl4.Bounds.IntersectsWith(lbl2x4.Bounds) Or lbl4.Bounds.IntersectsWith(lbl2x5.Bounds) Then
+            lbl4.ForeColor = Color.Red
+            lbl4.Location = New Point(165, 153)
+            '        limit()
+        End If
+        If counter <= 5 Then
+            move = True
+        End If
+        lblCounter.Text = counter
+    End Sub
+    Private Sub reset()
+        Static move As Boolean = True
+        lbl2.ForeColor = Color.Black
+        lbl4.ForeColor = Color.Black
+        lbl6.ForeColor = Color.Black
+        lbl8.ForeColor = Color.Black
+        lbl10.ForeColor = Color.Black
+
+        lbl2x1.ForeColor = Color.Black
+        lbl2x2.ForeColor = Color.Black
+        lbl2x3.ForeColor = Color.Black
+        lbl2x4.ForeColor = Color.Black
+        lbl2x5.ForeColor = Color.Black
+
+        lbl2.Location = New Point(165, 127)
+        lbl4.Location = New Point(165, 153)
+        lbl6.Location = New Point(165, 41)
+        lbl8.Location = New Point(165, 97)
+        lbl10.Location = New Point(165, 69)
+
+        counter = 0
+    End Sub
+    Private Sub limit()
+        If counter = 3 Then
+            MsgBox("You got 3 incorrect, try again.")
+            Exit Sub
+        End If
+    End Sub
+    Private Sub win()
+        If counter = 5 Then
+            MsgBox("You Win!")
+            reset()
+        End If
+    End Sub
+End Class
